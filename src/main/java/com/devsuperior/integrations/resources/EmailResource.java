ad273c91a5f18f0c02ee53633c95e8441d3c7226
João Pedro <jpedrodaibello1999@outlook.com>
2021-09-08 19:43:49 -0300

    Sending e-mail

--- src/main/java/com/devsuperior/integrations/resources/EmailResource.java
+++ src/main/java/com/devsuperior/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.devsuperior.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,12 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.integrations.dto.EmailDTO;
+import com.devsuperior.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
+	@Autowired
+	private EmailService service;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
